cd3cd28b422a7644001c16c85a761d2cfef8c41b
raviattrash-pro <raviattrash1@gmail.com>
2026-02-04 15:32:49 +0530

    Fix duplicate constructor in AuthController

--- auth-service/src/main/java/com/library/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/library/auth/controller/AuthController.java
@@ -24,10 +24,6 @@ public class AuthController {
 
     private final AuthService authService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
-
     /**
      * User login
      */
